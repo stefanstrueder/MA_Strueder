a63c4e155d22cda74bc71246360e7c0ae9b8f3ca
Stefan Hermann Strüder <stefanstrueder@uni-koblenz.de>
2020-03-01 11:54:19 +0100

    Added evaluation files.

---
+++ Evaluation/Skripte/Converter.py
@@ -0,0 +1,59 @@
+# Script to crawl for dates of each commit.
+# Results will be stored in MySQL database.
+# Usage of external library MySQL Connector.
+# Usage of external library PyDriller.
+# By Stefan Strueder, 2020.
+
+from pydriller import RepositoryMining
+import mysql.connector
+
+# Initialize connection to mysql database
+target_db = mysql.connector.connect(host = "localhost", user = "root", passwd = "*****", database = "dataset_evaluation")
+mycursor = target_db.cursor()
+
+softwares = ["busybox", "emacs", "gimp", "gnumeric", "gnuplot", "irssi", "libxml2", "lighttpd", "mpsolve", "parrot", "vim", "xfig"]
+#softwares = ["blender1", "blender2", "blender3", "blender4"]
+
+# Crawl for each software project and enter results in new table
+for software in softwares:
+	query1 = "SELECT distinct(commit_hash) FROM " + software
+
+	mycursor.execute(query1)
+	result_set = mycursor.fetchall()
+
+	counter = 0
+	
+	hash_list = []
+	date_list = []
+	id_list = []
+	
+	for row in result_set:
+		hash = str(row[0])
+		id_list.append(hash)
+
+	for commit in RepositoryMining("blender", only_commits = id_list).traverse_commits():
+		date = commit.committer_date
+		hash_list.append(commit.hash)
+		date_list.append(str(date))
+		counter = counter + 1
+		print("Sth happening: " + str(counter) + " " + software)
+		
+	query2 = "SELECT * from " + software
+	
+	mycursor.execute(query2)
+	result_set2 = mycursor.fetchall()
+	length = len(result_set2)
+	
+	for elem in result_set2:
+		id = elem[2]
+		
+		index = hash_list.index(id)
+		date = date_list[index]
+	
+		query3 = "INSERT INTO " + software + "_new (name, release_number, commit_hash, commit_author, commit_msg, filename, nloc, cycomplexity, lines_added, lines_removed, change_type, diff, corrective, bug_introducing, refactor, feature, date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+		val = (elem[0], elem[1], elem[2], elem[3], elem[4], elem[5], elem[6], elem[7], elem[8], elem[9], elem[10], elem[11], elem[12], elem[13], elem[13], elem[14], date)
+		mycursor.execute(query3, val)
+		target_db.commit()
+		length = length - 1
+		print("Entering " + id + " " + software + " " + str(length))
---
+++ Evaluation/Skripte/FileDataset.py
@@ -0,0 +1,51 @@
+# Script to create final table.
+# Results will be stored in MySQL database.
+# Usage of external library MySQL Connector.
+# By Stefan Strueder, 2020.
+
+import mysql.connector
+
+# Initialize connection to mysql database
+target_db = mysql.connector.connect(host = "localhost", user = "root", passwd = "*****", database = "dataset_evaluation")
+mycursor = target_db.cursor()
+
+# List of involved software projects 
+softwares = ["blender", "busybox", "emacs", "gimp", "gnumeric", "gnuplot", "irssi", "libxml2", "lighttpd", "mpsolve", "parrot", "vim", "xfig"]
+
+# Extract metrics and enter in final table
+for software in softwares:
+	
+	query1 = "SELECT * FROM " + software + "_final_new"
+	
+	mycursor.execute(query1)
+	result_set = mycursor.fetchall()
+	
+	counter = 0
+	
+	for elem in result_set:
+		revi = elem[3]
+		refa = elem[4]
+		bugf = elem[5]
+		auth = elem[6]
+		addl = elem[7]
+		addm = elem[8]
+		adda = elem[9]
+		reml = elem[10]
+		remm = elem[11]
+		rema = elem[12]
+		cchl = elem[13]
+		cchm = elem[14]
+		ccha = elem[15]
+		maxc = elem[16]
+		avgc = elem[17]
+		aage = elem[18]
+		wage = elem[19]
+		label = elem[20]
+
+		query2 = "INSERT INTO final (revi, refa, bugf, auth, addl, addm, adda, reml, remm, rema, cchl, cchm, ccha, maxc, avgc, aage, wage, label) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+		val = (revi, refa, bugf, auth, addl, addm, adda, reml, remm, rema, cchl, cchm, ccha, maxc, avgc, aage, wage, label)
+
+		mycursor.execute(query2, val)
+		target_db.commit()
+		counter = counter + 1
+		print("Sth happening for " + software + ": " + str(counter))
